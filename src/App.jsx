38dc32eb937d0fd6dc132d2c25756aabcf3150c1
Yulianny Betancourt <flipsdulce@gmail.com>
2022-12-22 15:32:08 -0500

    :white_check_mark: Form and input to add gifts

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Form } from './components/Form'
 import { ListGift } from './components/ListGift'
 
 export const App = () => {
   return (
     <div className="App">
     <h1>Adviency Calendar</h1>
+    <Form />
     <ListGift />
     </div>
   )
---
+++ src/components/Form.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Form = () => {
+  return (
+    <form action="">
+        <input type="text" placeholder='Nuevo regalo'/>
+        <button>AGREGAR</button>
+    </form>
+  )
+}
