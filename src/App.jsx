c69fbc8ebe1afdad62114bfe1139a2a1565263ea
Yulianny Betancourt <flipsdulce@gmail.com>
2022-12-22 14:45:00 -0500

    :sparkles: Init day 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { ListGift } from './components/ListGift'
+
 export const App = () => {
   return (
     <div className="App">
     <h1>Adviency Calendar</h1>
+    <ListGift />
     </div>
   )
 }
---
+++ src/helpers/data.js
@@ -0,0 +1,22 @@
+export const Gifts = [
+  {
+    id: 1,
+    item: "Guitar",
+    price: 100,
+  },
+  {
+    id: 2,
+    item: "Xbox",
+    price: 200,
+  },
+  {
+    id: 3,
+    item: "I phone",
+    price: 99,
+  },
+  {
+    id: 4,
+    item: "Smart TV",
+    price: 50,
+  },
+];
